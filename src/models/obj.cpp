499e2a5f24c2474a62a29cba73d2ec941f3b44d8
segovia benjamin <segovia.benjamin@gmail.com>
2011-12-03 07:40:47 +0000

    Remove the custom STL. It is just not enough and too ugly

--- src/models/obj.cpp
+++ src/models/obj.cpp
@@ -23,12 +23,9 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cassert>
-//#include <map>
-//#include <vector>
-//#include <algorithm>
-#include "stl/map.hpp"
-#include "stl/vector.hpp"
-#include "stl/algorithm.hpp"
+#include <map>
+#include <vector>
+#include <algorithm>
 
 #if defined (__MSVC__)
 #define strtok_r(a,b,c) strtok_s(a,b,c)
@@ -122,11 +119,11 @@ namespace pf
       for (int i = 0; i < MAX_VERT_NUM; i++)
         indices[i] = this->getListIndex(current_max, indices[i]);
     }
-    vector<ObjLoaderVec*>  vertexList;   //!< All positions parsed
-    vector<ObjLoaderVec*>  normalList;   //!< All normals parsed
-    vector<ObjLoaderVec*>  textureList;  //!< All textures parsed
-    vector<ObjLoaderFace*> faceList;     //!< All faces parsed
-    vector<ObjLoaderMat*>  materialList; //!< All materials parsed
+    std::vector<ObjLoaderVec*>  vertexList;   //!< All positions parsed
+    std::vector<ObjLoaderVec*>  normalList;   //!< All normals parsed
+    std::vector<ObjLoaderVec*>  textureList;  //!< All textures parsed
+    std::vector<ObjLoaderFace*> faceList;     //!< All faces parsed
+    std::vector<ObjLoaderMat*>  materialList; //!< All materials parsed
     GrowingPool<ObjLoaderFace>  faceAllocator;   //!< Speeds up face allocation
     GrowingPool<ObjLoaderVec>   vectorAllocator; //!< Speeds up vector allocation
     GrowingPool<ObjLoaderMat>   matAllocator;    //!< Speeds up material allocation
@@ -451,8 +448,8 @@ namespace pf
   bool Obj::load(const FileName &fileName)
   {
     ObjLoader loader;
-    map<VertexKey, int> map;
-    vector<Poly> polys;
+    std::map<VertexKey, int> map;
+    std::vector<Poly> polys;
     if (loader.loadObj(fileName.c_str()) == 0) return false;
 
     int vert_n = 0;
@@ -486,7 +483,7 @@ namespace pf
     if (polys.size() == 0) return true;
 
     // Create triangles now
-    vector<Triangle> tris;
+    std::vector<Triangle> tris;
     for (auto poly = polys.begin(); poly != polys.end(); ++poly) {
       if (poly->n == 3) {
         const Triangle tri(vec3i(poly->v[0], poly->v[1], poly->v[2]), poly->mat);
@@ -500,8 +497,8 @@ namespace pf
     }
 
     // Sort them by material and save the material group
-    quick_sort(tris.begin(), tris.end(), cmp);
-    vector<MatGroup> matGrp;
+    std::sort(tris.begin(), tris.end(), cmp);
+    std::vector<MatGroup> matGrp;
     int curr = tris[0].m;
     matGrp.push_back(MatGroup(0,0,curr));
     for (size_t i = 0; i < tris.size(); ++i) {
@@ -534,7 +531,7 @@ namespace pf
 
     // Create all the vertices and store them
     const size_t vertNum = map.size();
-    vector<Vertex> verts;
+    std::vector<Vertex> verts;
     verts.resize(vertNum);
     bool allPositionSet = true, allNormalSet = true, allTexCoordSet = true;
     for (auto it = map.begin(); it != map.end(); ++it) {
